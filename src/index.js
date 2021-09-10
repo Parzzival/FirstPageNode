e63cd8156819bea916e75c40eef6193a60312eb0
Cesar <cesartetsu@gmail.com>
2021-09-10 18:56:10 -0500

    min 1:04:17 curse nodeJs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-const express = require('express')
+const { renderFile } = require('ejs');
+const express = require('express');
 const app = express();
+const path = require('path');
+
 
 //setting
 app.set('port', 3000);
+// configuration for the rout
+app.set('views', path.join(__dirname, 'views'));
+// configuration for use ejs -> html
+app.engine('html', require('ejs').renderFile);
+// view motor for use ejs
+app.set('view engine', 'ejs');
+
 //middlewares
 //app.all();
 
 //routes
-app.get('/', (req, res) =>{
-    res.send('hello world')
-});
+app.use(require('./routes/index'));
+
 
 //static files
+app.use(express.static(path.join(__dirname, 'public')));
 //listening server
 app.listen(app.get('port'), () =>{
     console.log('on server', app.get('port'));
